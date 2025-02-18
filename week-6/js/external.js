7af7157bd241b01bb2dc7bd7abf1958f42118aa8
JoseMedina <medinaja@iu.edu>
2025-02-18 11:25:51 -0500

    Week-6 class

---
+++ week-6/js/external.js
@@ -0,0 +1,21 @@
+console.log("Umm... where are we?");
+
+const birthYear = 1207;
+
+let myName = "Ty the noodle Arm Guy";
+
+console.log(myName, "was born",birthYear);
+
+myName = "Ty the Fly Guy";
+
+console.warn(myName, "MAY have devoured 10 souls");
+
+console.log("myName is", myName.length,"characters long");
+
+
+console.log("myName includes ty", myName.toUpperCase().includes("TY"));
+
+myName = "Ty"
+
+console.log("myName has uppercase:", myName !== myName.toLowerCase());
+console.log("myName has lowercase:", myName !== myName.toUpperCase());
